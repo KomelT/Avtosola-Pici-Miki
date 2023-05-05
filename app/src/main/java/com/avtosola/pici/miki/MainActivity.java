f93e38f1da0ae46695332eb9f07a8245a7580401
Tilen Komel <tilen.komel10@gmail.com>
2023-05-05 13:08:02 +0200

    Add register page

--- app/src/main/java/com/avtosola/pici/miki/MainActivity.java
+++ app/src/main/java/com/avtosola/pici/miki/MainActivity.java
@@ -3,6 +3,9 @@ package com.avtosola.pici.miki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_registracija = (Button) findViewById(R.id.register);
+        bt_registracija.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }

09b1d53af1a5011f657293bf267d9e58444c111a
Tilen Komel <tilen.komel10@gmail.com>
2023-05-20 18:11:14 +0200

    Fix multiplying buttons when going back

--- app/src/main/java/com/avtosola/pici/miki/SvPrijaviSeNaCPPIzpitInformacijskiSistemUprave.java
+++ app/src/main/java/com/avtosola/pici/miki/SvPrijaviSeNaCPPIzpitInformacijskiSistemUprave.java
@@ -7,6 +7,8 @@ public class SvPrijaviSeNaCPPIzpitInformacijskiSistemUprave {
       Lokacija Ljubljana = new Lokacija("Upravna enota Tobačna Ljubljana", "Tobačna 69, 1000 Ljubljana");
       Lokacija Maribor = new Lokacija("Upravna enota Maribor", "Ulica Totega Štajrca, 2000 Maribor");
 
+      KPrijaviSeNaCPPIzpit.lokacija.clear();
+
       KPrijaviSeNaCPPIzpit.lokacija.add(NovaGorica);
       KPrijaviSeNaCPPIzpit.lokacija.add(Ljubljana);
       KPrijaviSeNaCPPIzpit.lokacija.add(Maribor);
@@ -15,6 +17,8 @@ public class SvPrijaviSeNaCPPIzpitInformacijskiSistemUprave {
       Termini dopoldne7 = new Termini("22.9.2023", "7:00", 45.0);
       Termini dopoldne9 = new Termini("22.9.2023", "9:00", 45.0);
       Termini popoldne13 = new Termini("22.9.2023", "13:00", 45.0);
+
+      KPrijaviSeNaCPPIzpit.termini.clear();
 
       KPrijaviSeNaCPPIzpit.termini.add(dopoldne7);
       KPrijaviSeNaCPPIzpit.termini.add(dopoldne9);
